0c8dea1e0dd029cb190b0bdd8d1952c5a14ed182
Assan <adinanemalanbaba8@gmail.com>
2026-07-11 20:45:42 +0600

    task11: генерация класса Calculator во время выполнения через Roslyn

---
+++ task11/RuntimeClassGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace task11;
+
+public static class RuntimeClassGenerator
+{
+    public static ICalculator CreateCalculator()
+    {
+        var sourceCode = @"
+using task11;
+
+public class Calculator : ICalculator
+{
+    public int Add(int a, int b) => a + b;
+    public int Minus(int a, int b) => a - b;
+    public int Mul(int a, int b) => a * b;
+    public int Div(int a, int b) => a / b;
+}
+";
+
+        var syntaxTree = CSharpSyntaxTree.ParseText(sourceCode);
+
+        var references = AppDomain.CurrentDomain.GetAssemblies()
+            .Where(a => !a.IsDynamic && !string.IsNullOrEmpty(a.Location))
+            .Select(a => MetadataReference.CreateFromFile(a.Location))
+            .Cast<MetadataReference>()
+            .ToList();
+
+        var compilation = CSharpCompilation.Create(
+            "DynamicCalculatorAssembly",
+            new[] { syntaxTree },
+            references,
+            new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+        using var ms = new MemoryStream();
+        var result = compilation.Emit(ms);
+
+        if (!result.Success)
+        {
+            var errors = string.Join(Environment.NewLine, result.Diagnostics.Where(d => d.Severity == DiagnosticSeverity.Error));
+            throw new InvalidOperationException("Ошибка компиляции сгенерированного класса: " + errors);
+        }
+
+        ms.Seek(0, SeekOrigin.Begin);
+        var assembly = Assembly.Load(ms.ToArray());
+
+        var calculatorType = assembly.GetType("Calculator");
+        var instance = Activator.CreateInstance(calculatorType!);
+
+        return (ICalculator)instance!;
+    }
+}
---
+++ task11tests/RuntimeClassGeneratorTests.cs
@@ -0,0 +1,63 @@
+﻿using Xunit;
+
+namespace task11tests;
+
+public class RuntimeClassGeneratorTests
+{
+    [Fact]
+    public void CreateCalculator_ReturnsWorkingInstance()
+    {
+        var calculator = task11.RuntimeClassGenerator.CreateCalculator();
+
+        Assert.NotNull(calculator);
+        Assert.IsAssignableFrom<task11.ICalculator>(calculator);
+    }
+
+    [Fact]
+    public void Add_ReturnsCorrectSum()
+    {
+        var calculator = task11.RuntimeClassGenerator.CreateCalculator();
+
+        var result = calculator.Add(3, 5);
+
+        Assert.Equal(8, result);
+    }
+
+    [Fact]
+    public void Minus_ReturnsCorrectDifference()
+    {
+        var calculator = task11.RuntimeClassGenerator.CreateCalculator();
+
+        var result = calculator.Minus(10, 4);
+
+        Assert.Equal(6, result);
+    }
+
+    [Fact]
+    public void Mul_ReturnsCorrectProduct()
+    {
+        var calculator = task11.RuntimeClassGenerator.CreateCalculator();
+
+        var result = calculator.Mul(6, 7);
+
+        Assert.Equal(42, result);
+    }
+
+    [Fact]
+    public void Div_ReturnsCorrectQuotient()
+    {
+        var calculator = task11.RuntimeClassGenerator.CreateCalculator();
+
+        var result = calculator.Div(20, 4);
+
+        Assert.Equal(5, result);
+    }
+
+    [Fact]
+    public void Div_ByZero_ThrowsDivideByZeroException()
+    {
+        var calculator = task11.RuntimeClassGenerator.CreateCalculator();
+
+        Assert.Throws<DivideByZeroException>(() => calculator.Div(10, 0));
+    }
+}
