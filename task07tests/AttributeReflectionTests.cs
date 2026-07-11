47ac8a6d376c807d6fc389d004ae4e5b1742a473
Assan <adinanemalanbaba8@gmail.com>
2026-07-11 18:17:51 +0600

    task07: реализация атрибутов DisplayName, Version и ReflectionHelper

---
+++ task07tests/AttributeReflectionTests.cs
@@ -0,0 +1,51 @@
+﻿using Xunit;
+using System.Reflection;
+
+namespace task07tests;
+
+public class AttributeReflectionTests
+{
+    [Fact]
+    public void Class_HasDisplayNameAttribute()
+    {
+        var type = typeof(task07.SampleClass);
+        var attribute = type.GetCustomAttribute<task07.DisplayNameAttribute>();
+        Assert.NotNull(attribute);
+        Assert.Equal("Пример класса", attribute.DisplayName);
+    }
+
+    [Fact]
+    public void Method_HasDisplayNameAttribute()
+    {
+        var method = typeof(task07.SampleClass).GetMethod("TestMethod");
+        var attribute = method.GetCustomAttribute<task07.DisplayNameAttribute>();
+        Assert.NotNull(attribute);
+        Assert.Equal("Тестовый метод", attribute.DisplayName);
+    }
+
+    [Fact]
+    public void Property_HasDisplayNameAttribute()
+    {
+        var prop = typeof(task07.SampleClass).GetProperty("Number");
+        var attribute = prop.GetCustomAttribute<task07.DisplayNameAttribute>();
+        Assert.NotNull(attribute);
+        Assert.Equal("Числовое свойство", attribute.DisplayName);
+    }
+
+    [Fact]
+    public void Class_HasVersionAttribute()
+    {
+        var type = typeof(task07.SampleClass);
+        var attribute = type.GetCustomAttribute<task07.VersionAttribute>();
+        Assert.NotNull(attribute);
+        Assert.Equal(1, attribute.Major);
+        Assert.Equal(0, attribute.Minor);
+    }
+
+    [Fact]
+    public void PrintTypeInfo_DoesNotThrow()
+    {
+        var exception = Record.Exception(() => task07.ReflectionHelper.PrintTypeInfo(typeof(task07.SampleClass)));
+        Assert.Null(exception);
+    }
+}
