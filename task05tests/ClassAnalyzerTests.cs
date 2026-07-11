9162bc947dbbf6d23303496be1258ea7c9cb9662
Assan <adinanemalanbaba8@gmail.com>
2026-07-11 14:09:58 +0600

    task05: реализация ClassAnalyzer с использованием рефлексии

---
+++ task05tests/ClassAnalyzerTests.cs
@@ -0,0 +1,84 @@
+﻿using Xunit;
+using task05;
+
+namespace task05tests;
+
+public class TestClass
+{
+    public int PublicField;
+    private string _privateField = string.Empty;
+    public int Property { get; set; }
+
+    public void Method() { }
+
+    public int Add(int a, int b) => a + b;
+}
+
+[Serializable]
+public class AttributedClass { }
+
+public class ClassAnalyzerTests
+{
+    [Fact]
+    public void GetPublicMethods_ReturnsCorrectMethods()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var methods = analyzer.GetPublicMethods();
+
+        Assert.Contains("Method", methods);
+    }
+
+    [Fact]
+    public void GetAllFields_IncludesPrivateFields()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var fields = analyzer.GetAllFields();
+
+        Assert.Contains("_privateField", fields);
+    }
+
+    [Fact]
+    public void GetAllFields_IncludesPublicFields()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var fields = analyzer.GetAllFields();
+
+        Assert.Contains("PublicField", fields);
+    }
+
+    [Fact]
+    public void GetProperties_ReturnsCorrectProperties()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var properties = analyzer.GetProperties();
+
+        Assert.Contains("Property", properties);
+    }
+
+    [Fact]
+    public void GetMethodParams_ReturnsParameterNamesAndReturnType()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var parameters = analyzer.GetMethodParams("Add").ToList();
+
+        Assert.Contains("a", parameters);
+        Assert.Contains("b", parameters);
+        Assert.Contains("Int32", parameters);
+    }
+
+    [Fact]
+    public void HasAttribute_ReturnsTrue_WhenAttributePresent()
+    {
+        var analyzer = new ClassAnalyzer(typeof(AttributedClass));
+
+        Assert.True(analyzer.HasAttribute<SerializableAttribute>());
+    }
+
+    [Fact]
+    public void HasAttribute_ReturnsFalse_WhenAttributeAbsent()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+
+        Assert.False(analyzer.HasAttribute<SerializableAttribute>());
+    }
+}
