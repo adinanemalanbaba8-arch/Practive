1b3305cb81843f9c5263d35b05c4784a5e64fc13
Assan <adinanemalanbaba8@gmail.com>
2026-07-16 14:28:12 +0600

    task19: demonstration ServerThread avec 5 TestCommand en Round Robin

---
+++ practice2026/task19/Program.cs
@@ -0,0 +1,26 @@
+﻿using Task17;
+using Task18;
+using Task19;
+
+var scheduler = new RoundRobinScheduler();
+var server = new SchedulingServerThread(scheduler);
+
+var commands = new List<TestCommand>();
+for (int i = 1; i <= 5; i++)
+{
+    var cmd = new TestCommand(i);
+    commands.Add(cmd);
+    server.Enqueue(cmd);
+}
+
+while (commands.Exists(c => !c.IsCompleted))
+{
+    Thread.Sleep(20);
+}
+
+Console.WriteLine("Toutes les commandes ont termine leurs 3 executions. Arret du thread (HardStop).");
+
+server.Stop();
+server.Join(TimeSpan.FromSeconds(2));
+
+Console.WriteLine("Thread arrete.");
